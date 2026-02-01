1a5295e5da0ecf352d254259fb05f8f099cbf5e2
Fazzelis <odidk08@gmail.com>
2026-02-01 20:16:03 +0700

    Добавлено поле jti

--- backend/models/refresh_token.py
+++ backend/models/refresh_token.py
@@ -10,6 +10,7 @@ class RefreshToken(Base):
 
     user_id = Column(UUID(as_uuid=True), ForeignKey("user.id", ondelete="CASCADE"))
     token_hash = Column(String, nullable=False, unique=True)
+    jti = Column(UUID(as_uuid=True), nullable=False, unique=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     expires_at = Column(DateTime(timezone=True), nullable=False)
     revoked_at = Column(DateTime(timezone=True), nullable=True)
