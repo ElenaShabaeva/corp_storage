059e0eacc41db4928d38d82fcc7eb2e308fd1c80
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-02-02 22:20:33 +0700

    изменен путь у страницы регистрации

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
+      path: "/registration",
       name: "registration",
       component: () => import("@/pages/RegistrationPage.vue"),
       meta: { requiresAuth: false },
