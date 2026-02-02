9e28122f95ed0a35e5f968bc41b2e69493f09508
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-02-02 22:15:05 +0700

    подключены components, router и pinia в main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,17 @@
 import '@/styles/styles.less'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import components from "@/components/UI";
+import router from './router';
+
+const app = createApp(App);
+const pinia = createPinia()
+
+components.forEach((component) => {
+  app.component(component.name, component);
+});
+
+app.use(pinia).use(router).mount('#app')
