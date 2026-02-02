fa17c0bd0b5d5f364ea2522e224ea70b518f013c
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-02-02 22:20:52 +0700

    header и страницы подключены в App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,10 @@
 <template>
-  
+  <PageHeader />
+  <main>
+      <router-view></router-view>
+  </main>
 </template>
+
+<script setup>
+import PageHeader from './components/PageHeader.vue';
+</script>
