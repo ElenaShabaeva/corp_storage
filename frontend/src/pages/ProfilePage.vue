31174d3fa65bfe31ca5afd54a42474c8fdfe3dc2
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-02-04 18:56:34 +0700

    изменена страница профиля и добавлен подгрузка данных

--- frontend/src/pages/ProfilePage.vue
+++ frontend/src/pages/ProfilePage.vue
@@ -1,23 +1,49 @@
 <template>
   <div class="profile">
-    <h1 class="title">Профиль</h1>
-    <div class="profile__block">
-      <ProfileForm />
+    <ProfileSkeleton v-if="store.initialLoading" />
+    <div
+      class="profile__wrapper"
+      v-else
+    >
+      <div class="profile__data">
+        <h1 class="title">Профиль</h1>
+        <ProfileForm />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import ProfileForm from '../components/ProfileForm.vue';
+import { onMounted } from "vue";
+import ProfileForm from "../components/ProfileForm.vue";
+import ProfileSkeleton from "../components/skeleton/ProfileSkeleton.vue";
+import { useProfileStore } from "../store/profile";
 
+const store = useProfileStore();
+
+onMounted(async () => {
+  await store.getProfile();
+});
 </script>
 
 <style lang="less">
-.profile{
-  &__block{
+.profile {
+  &__wrapper {
     display: flex;
     flex-direction: column;
     row-gap: 100px;
   }
 }
+
+.modal-move,
+.modal-enter-active,
+.modal-leave-active {
+  transition: all 0.4s ease;
+}
+.modal-enter-from,
+.modal-leave-to {
+  opacity: 0;
+  transform: translateY(-92px);
+}
+
 </style>
