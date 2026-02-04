edba77f8eeb7e5280a539a75af04a78dbe62dffc
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-02-04 15:48:34 +0700

    добавлена страница профиля

---
+++ frontend/src/pages/ProfilePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="profile">
+    <h1 class="title">Профиль</h1>
+    <div class="profile__block">
+      <ProfileForm />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ProfileForm from '../components/ProfileForm.vue';
+
+</script>
+
+<style lang="less">
+.profile{
+  &__block{
+    display: flex;
+    flex-direction: column;
+    row-gap: 100px;
+  }
+}
+</style>
