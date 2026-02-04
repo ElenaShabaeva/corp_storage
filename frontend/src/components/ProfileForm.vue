feb93da613f6ff6bdae07d3d34e96528ef17f915
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-02-04 15:48:25 +0700

    добавлена форма данных в профиле

---
+++ frontend/src/components/ProfileForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <form class="form form--row" @submit.prevent="handleUpdate">
+    <div class="form__fields">
+      <my-field
+        :label="'Имя'"
+        :type="'text'"
+        :placeholder="'Иван'"
+      />
+      <my-field
+        :label="'Фамилия'"
+        :type="'text'"
+        :placeholder="'Иванов'"
+      />
+      <my-field
+        :label="'Логин'"
+        :type="'text'"
+        :placeholder="'Иван'"
+        :disabled="true"
+        
+      />
+    </div>
+    <div class="form__buttons">
+        <my-button :filled="true" type="submit" 
+          >Обновить данные</my-button
+        >
+        <my-button type="button" 
+          >Удалить аккаунт</my-button
+        >
+    </div>
+  </form>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less">
+
+</style>
