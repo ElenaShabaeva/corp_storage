c2811a3cec371b5dfb4bdf23052e98d05e0811d8
Fazzelis <odidk08@gmail.com>
2026-02-08 18:41:02 +0700

    Убраные строки в конфигурации

--- backend/configuration.py
+++ backend/configuration.py
@@ -4,8 +4,6 @@ from fastapi.security import HTTPBearer
 
 
 class Settings(BaseSettings):
-    url: str = "http://127.0.0.1:8000"
-    get_attachment_url: str = f"{url}/attachment?attachment_id="
     expiration_time_of_access_token: int = 30
     expiration_time_of_refresh_token: int = 20160
     expiration_time_of_access_token_for_browser: int = expiration_time_of_access_token*60
