b116ad227ec2ec8ac6e5300bf9d6c01cde6e67fd
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-02-08 18:52:43 +0700

    добавлен отображение ошибок и загрузочных экранов при обновлении и удалении

--- frontend/src/pages/ProfilePage.vue
+++ frontend/src/pages/ProfilePage.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="profile">
     <ProfileSkeleton v-if="store.initialLoading" />
-    <div
-      class="profile__wrapper"
-      v-else
-    >
+    <div class="profile__wrapper" v-else>
       <div class="profile__data">
         <h1 class="title">Профиль</h1>
-        <ProfileForm />
+        <ProfileForm/>
       </div>
     </div>
   </div>
+  <Loading :title="'Идет обновление данных'" v-if="store.updateLoading" />
+  <Loading :title="'Идет удаление аккаунта'" v-if="store.deleteLoading" />
+  <DeleteProfile
+    v-if="store.showDeleteModal"
+  />
+  <Transition name="modal" appear v-if="store.serverError">
+    <div class="modal" :class="{ 'modal--error': store.serverError }">
+      {{ store.serverError }}
+    </div>
+  </Transition>
+  <Transition name="modal" appear v-if="store.success">
+    <div class="modal" :class="{ 'modal--success': store.success }">
+      {{ store.success }}
+    </div>
+  </Transition>
 </template>
 
 <script setup>
@@ -18,6 +30,8 @@ import { onMounted } from "vue";
 import ProfileForm from "../components/ProfileForm.vue";
 import ProfileSkeleton from "../components/skeleton/ProfileSkeleton.vue";
 import { useProfileStore } from "../store/profile";
+import DeleteProfile from "../components/DeleteProfile.vue";
+import Loading from "../components/Loading.vue";
 
 const store = useProfileStore();
 
@@ -46,4 +60,9 @@ onMounted(async () => {
   transform: translateY(-92px);
 }
 
+.modal-enter-to,
+.modal-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
 </style>
