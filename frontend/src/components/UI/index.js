2ff82e875093b8cbce9d2159f70415b1a3ac1a62
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-02-10 13:23:12 +0700

    в index.js добавлен компонент MyTextButton

--- frontend/src/components/UI/index.js
+++ frontend/src/components/UI/index.js
@@ -1,9 +1,11 @@
 import MyButton from "./MyButton.vue";
 import MyField from "./MyField.vue";
 import MyInput from "./MyInput.vue";
+import MyTextButton from "./MyTextButton.vue";
 
 export default [
     MyInput,
     MyField,
-    MyButton
+    MyButton,
+    MyTextButton,
 ];
