a37b79abfe6477fdc403211addcc1f2033fedda8
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-02-10 15:15:08 +0700

    в ProjectsPage добавлена pp-форма создания проекта

--- frontend/src/pages/ProjectsPage.vue
+++ frontend/src/pages/ProjectsPage.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="projects">
     <div class="projects__wrapper">
-      <my-button :filled="true" type="button">Создать проект</my-button>
+      <my-button :filled="true" type="button" @click="showCreateModal">Создать проект</my-button>
       <ProjectsListing class="projects__listing"/>
     </div>
   </div>
+  <CreateProjectsForm v-if="store.showCreateModal"/>
 </template>
 
 <script setup>
+import CreateProjectsForm from '../components/CreateProjectsForm.vue';
 import ProjectsListing from '../components/ProjectsListing.vue';
+import { useProjectsStore } from '../store/projects';
 
+const store = useProjectsStore()
+
+async function showCreateModal() {
+  store.openCreateModal()
+}
 </script>
 
 <style lang="less">
