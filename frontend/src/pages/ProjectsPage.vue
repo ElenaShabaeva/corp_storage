9c20f671c004ad072d917cff08957ffdab0287a3
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-02-10 13:22:48 +0700

    добавлена страница проектов

---
+++ frontend/src/pages/ProjectsPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="projects">
+    <div class="projects__wrapper">
+      <my-button :filled="true" type="button">Создать проект</my-button>
+      <ProjectsListing class="projects__listing"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ProjectsListing from '../components/ProjectsListing.vue';
+
+</script>
+
+<style lang="less">
+.projects {
+  &__wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+    row-gap: 48px;
+  }
+
+  &__listing{
+    width: 100%;
+  }
+}
+</style>
