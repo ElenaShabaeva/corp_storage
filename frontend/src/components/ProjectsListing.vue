3164cafc783e3529d82e12975dd6957bf7f35bd7
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-02-10 13:22:23 +0700

    добавлен компонент ProjectListing

---
+++ frontend/src/components/ProjectsListing.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="projects-listing">
+    <h1 class="title">Проекты</h1>
+    <ul class="projects-listing__list">
+      <ProjectCard />
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import ProjectCard from "./ProjectCard.vue";
+</script>
+
+<style lang="less">
+.projects-listing {
+  &__body {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+
+  &__list {
+    display: flex;
+    flex-direction: column;
+    row-gap: 16px;
+  }
+}
+</style>
