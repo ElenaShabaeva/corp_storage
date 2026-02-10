f15f72fcbcebd126eb845762620342f6d61f7132
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-02-10 18:07:55 +0700

    добавлен loading и модальное окно на страницу проектов

--- frontend/src/pages/ProjectsPage.vue
+++ frontend/src/pages/ProjectsPage.vue
@@ -6,18 +6,30 @@
     </div>
   </div>
   <CreateProjectsForm v-if="store.showCreateModal"/>
+  <Loading :title="'Идет создание проекта'" v-if="store.createLoading" />
+  <Transition name="modal" appear v-if="store.serverError">
+    <div class="modal" :class="{ 'modal--error': store.serverError }">
+      {{ store.serverError }}
+    </div>
+  </Transition>
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
 import CreateProjectsForm from '../components/CreateProjectsForm.vue';
 import ProjectsListing from '../components/ProjectsListing.vue';
 import { useProjectsStore } from '../store/projects';
+import Loading from '../components/Loading.vue';
 
 const store = useProjectsStore()
 
 async function showCreateModal() {
   store.openCreateModal()
 }
+
+onMounted(async () => {
+  await store.getProjects();
+});
 </script>
 
 <style lang="less">
