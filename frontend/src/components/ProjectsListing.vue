b9320eb2f476cd588b1e106ddf911dd2af4de22a
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-02-10 18:08:25 +0700

    добавлено отображение сообщения, когда нет проектов, и передан prop в ProjectCard

--- frontend/src/components/ProjectsListing.vue
+++ frontend/src/components/ProjectsListing.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="projects-listing">
     <h1 class="title">Проекты</h1>
-    <ul class="projects-listing__list">
-      <ProjectCard />
+    <p class="projects-listing__none" v-if="!store.projects">У вас сейчас нет проектов</p>
+    <ul class="projects-listing__list" v-else="store.projects">
+      <ProjectCard v-for="project in store.projects" :key="project.id" :project="project"/>
     </ul>
   </div>
 </template>
 
 <script setup>
+import { useProjectsStore } from "../store/projects";
 import ProjectCard from "./ProjectCard.vue";
+
+const store = useProjectsStore()
 </script>
 
 <style lang="less">
 .projects-listing {
+  &__none{
+    font-size: 18px;
+    font-weight: 700;
+    color: @text-tertiary;
+  }
+
   &__body {
     display: flex;
     flex-direction: column;
