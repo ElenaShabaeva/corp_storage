4f466434d96173d4263b9888cc56ac2be2522959
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-02-10 15:14:31 +0700

    в index.js подключены новые компоненты

--- frontend/src/components/UI/index.js
+++ frontend/src/components/UI/index.js
@@ -1,6 +1,8 @@
 import MyButton from "./MyButton.vue";
 import MyField from "./MyField.vue";
+import MyFieldTextarea from "./MyFieldTextarea.vue";
 import MyInput from "./MyInput.vue";
+import MyTextarea from "./MyTextarea.vue";
 import MyTextButton from "./MyTextButton.vue";
 
 export default [
@@ -8,4 +10,6 @@ export default [
     MyField,
     MyButton,
     MyTextButton,
+    MyTextarea,
+    MyFieldTextarea
 ];
