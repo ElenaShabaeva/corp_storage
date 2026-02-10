a0f277904cb3ce53db892d8afe725a63bc6e33ff
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-02-10 13:22:14 +0700

    добавлен компонент ProjectCard

---
+++ frontend/src/components/ProjectCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <li class="project-card">
+    <p class="project-card__name">Проект 1</p>
+    <div class="project-card__body">
+      <div class="project-card__info">
+        <span>Создан: nickname</span>
+        <span>Участников: 0</span>
+      </div>
+      <my-text-button>Покинуть проект</my-text-button>
+    </div>
+  </li>
+</template>
+
+<script setup></script>
+
+<style lang="less">
+.project-card {
+  display: flex;
+  flex-direction: column;
+  padding: 20px 40px;
+  border-radius: 16px;
+  background-color: @light-blue;
+  cursor: pointer;
+
+  &:hover {
+    .project-card__name {
+      color: @blue;
+    }
+  }
+
+  &__name {
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 1.4;
+    margin-bottom: 24px;
+    transition: all 0.3s;
+  }
+
+  &__body {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    align-items: end;
+    column-gap: 20px;
+  }
+
+  &__info {
+    display: flex;
+    flex-direction: column;
+    row-gap: 8px;
+
+    span {
+      font-size: 14px;
+    }
+  }
+}
+</style>
