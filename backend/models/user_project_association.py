034ef337f7cfd2a55d42542ea9abded042540465
Fazzelis <odidk08@gmail.com>
2026-02-11 17:52:50 +0700

    Добавлен ondelete cascade для user_project_association

--- backend/models/user_project_association.py
+++ backend/models/user_project_association.py
@@ -13,8 +13,8 @@ from sqlalchemy import (
 class UserProjectAssociation(Base):
     __tablename__ = "user_project_association"
     id = Column(UUID(as_uuid=True), primary_key=True, server_default=func.gen_random_uuid())
-    user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"))
-    project_id = Column(UUID(as_uuid=True), ForeignKey("project.id"))
+    user_id = Column(UUID(as_uuid=True), ForeignKey("user.id", ondelete="CASCADE"))
+    project_id = Column(UUID(as_uuid=True), ForeignKey("project.id", ondelete="CASCADE"))
 
     user = relationship("User", back_populates="project_association")
     project = relationship("Project", back_populates="user_association")
