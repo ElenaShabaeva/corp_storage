1c5917f079848e41f638fef5a86dcb7aa7977167
Fazzelis <odidk08@gmail.com>
2026-02-12 15:21:41 +0700

    Добавлено получение информации о проекте по id

--- backend/services/project_service.py
+++ backend/services/project_service.py
@@ -3,12 +3,17 @@ from fastapi import HTTPException, status
 from utils.jwt_utils import decode_jwt
 from jwt import ExpiredSignatureError, DecodeError
 from utils.uow import UnitOfWork
+from uuid import UUID
 from schemas.request.project_request import (
     ProjectCreateRequestSchema
 )
+from schemas.internal.user_schema import (
+    UserShortInfoSchema
+)
 from schemas.response.project_response import (
     ProjectShortInfoResponseSchema,
-    GetAllProjectsResponseSchema
+    GetAllProjectsResponseSchema,
+    ProjectFullInfoResponseSchema
 )
 
 
@@ -77,6 +82,47 @@ class ProjectService:
                 creator_login=user.login,
                 members_count=new_project.members_count
             )
+        except ExpiredSignatureError:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Access token истек"
+            )
+        except DecodeError:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Неверный формат токена"
+            )
+
+    async def get_by_id(self, project_id: UUID, access_token: str | None):
+        if not access_token:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Access token не найден"
+            )
+        try:
+            user_id, _ = decode_jwt(token=access_token)
+            async with self.uow.start():
+                project = await self.uow.projects.get_by_id(project_id=project_id)
+                if not project:
+                    raise HTTPException(
+                        status_code=status.HTTP_404_NOT_FOUND,
+                        detail="Проект не найден"
+                    )
+                members = await self.uow.projects.get_members(project_id=project_id)
+                user = await self.uow.users.get_by_id(user_id=user_id)
+
+                return ProjectFullInfoResponseSchema(
+                    id=project.id,
+                    name=project.name,
+                    creator_login=user.login,
+                    members_count=project.members_count,
+                    isOwner=True if project.creator_id == user_id else False,
+                    description=project.description,
+                    members=[UserShortInfoSchema(
+                            id=member.id,
+                            login=member.login
+                        ) for member in members]
+                )
         except ExpiredSignatureError:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
