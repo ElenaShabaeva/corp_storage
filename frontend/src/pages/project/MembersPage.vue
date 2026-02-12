02d0c86c6b813a039183e2447ff0e5ce452189d3
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-02-12 21:50:42 +0700

    добавлен блок с участниками проекта

---
+++ frontend/src/pages/project/MembersPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="members">
+    <div class="members__top">
+      <span
+        >Участников в проекте:
+        {{ store.projectMembers?.members_count || 0 }}</span
+      >
+      <my-button type="button" :filled="true" v-if="store.projectOwner"
+        >Добавить участника</my-button
+      >
+    </div>
+    <ul class="members__list">
+      <MembersItem
+        v-for="member in store.projectMembers?.members"
+        :member="member"
+        :key="member.id"
+      />
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import MembersItem from "../../components/MembersItem.vue";
+import { useProjectsStore } from "../../store/projects";
+
+const store = useProjectsStore();
+</script>
+
+<style lang="less">
+.members {
+  display: flex;
+  flex-direction: column;
+  row-gap: 24px;
+
+  &__top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    column-gap: 20px;
+  }
+
+  &__list {
+    display: flex;
+    flex-direction: column;
+    row-gap: 24px;
+  }
+}
+</style>
