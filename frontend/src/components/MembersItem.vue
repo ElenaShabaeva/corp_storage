689e61c5951aa77effd200405c4ace31c05b3540
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-02-12 21:50:56 +0700

    добавлен компонент участника проекта

---
+++ frontend/src/components/MembersItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <li class="members-item">
+    <span>{{ member.login }}</span>
+    <my-text-button type="button" v-if="store.projectOwner">Исключить из проекта</my-text-button>
+  </li>
+</template>
+
+<script setup>
+import { useProjectsStore } from '../store/projects';
+
+const props = defineProps({
+  member: {
+    type: Object,
+  },
+  isOwner: Boolean
+});
+
+const store = useProjectsStore()
+</script>
+
+<style lang="less">
+.members-item {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  column-gap: 20px;
+  padding-left: 20px;
+
+  span {
+    &::before {
+      content: "";
+      position: absolute;
+      top: 7px;
+      left: 0;
+      width: 10px;
+      height: 10px;
+      border-radius: 50%;
+      background-color: @blue;
+    }
+  }
+}
+</style>
