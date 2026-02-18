470abff0fd91f0091526355198416329452142c5
Fazzelis <odidk08@gmail.com>
2026-02-18 12:52:05 +0700

    Переделан метод отправки приглашения в проект

--- backend/services/project_service.py
+++ backend/services/project_service.py
@@ -3,12 +3,18 @@ from utils.jwt_utils import decode_jwt
 from jwt import ExpiredSignatureError, DecodeError
 from utils.uow import UnitOfWork
 from uuid import UUID
+from datetime import datetime
+from services.notification_service import notification_service
 from schemas.request.project_request import (
     ProjectCreateRequestSchema,
     InviteKickUserRequestSchema
 )
 from schemas.internal.user_schema import (
     UserShortInfoSchema
+)
+from schemas.internal.notification_schema import (
+    InviteNotificationSchema,
+    MessageNotificationSchema
 )
 from schemas.response.project_response import (
     ProjectShortInfoResponseSchema,
@@ -177,6 +183,7 @@ class ProjectService:
             )
         try:
             user_id, _ = decode_jwt(token=access_token)
+
             async with self.uow.start():
                 project = await self.uow.projects.get_by_id(project_id=payload.project_id)
                 if project.creator_id != user_id:
@@ -184,6 +191,7 @@ class ProjectService:
                         status_code=status.HTTP_403_FORBIDDEN,
                         detail="Добавлять нового участника может только создатель проекта"
                     )
+
                 members = await self.uow.projects.get_members(project_id=payload.project_id)
                 new_user_in_project = await self.uow.users.get_by_login(login=payload.login)
                 if new_user_in_project.id in [member.id for member in members]:
@@ -191,11 +199,33 @@ class ProjectService:
                         status_code=status.HTTP_409_CONFLICT,
                         detail="Пользователь уже состоит в данном проекте"
                     )
-                await self.uow.user_project_association.add_member(
-                    user_id=new_user_in_project.id,
-                    project_id=payload.project_id
+
+                invite_notification = await self.uow.invite_notifications.get_by_user_and_project_ids(
+                    to_user_id=new_user_in_project.id,
+                    project_id=project.id
                 )
-                project.members_count += 1
+                if invite_notification:
+                    raise HTTPException(
+                        status_code=status.HTTP_409_CONFLICT,
+                        detail="Пользователь уже приглашен"
+                    )
+
+                creator = await self.uow.users.get_by_id(user_id=project.creator_id)
+                date_time = datetime.now()
+                await self.uow.invite_notifications.post(
+                    project_id=project.id,
+                    invite_datetime=date_time,
+                    from_user_id=user_id,
+                    to_user_id=new_user_in_project.id
+                )
+                await notification_service.send_notification(
+                    user_id=new_user_in_project.id,
+                    notification=InviteNotificationSchema(
+                        project_name=project.name,
+                        project_creator=creator.login,
+                        date_time=date_time.strftime("%d.%m.%Y / %H:%M")
+                    )
+                )
             return MessageResponseSchema(
                 status="success",
                 message="Приглашение в проект успешно отправлено"
