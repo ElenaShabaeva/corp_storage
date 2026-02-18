c8f066aa89802f38fe36084d790356067da7cb9c
Fazzelis <odidk08@gmail.com>
2026-02-18 23:36:20 +0700

    Добавлены id в схемы уведомлений

--- backend/schemas/internal/notification_schema.py
+++ backend/schemas/internal/notification_schema.py
@@ -1,12 +1,16 @@
 from pydantic import BaseModel
+from uuid import UUID
 
 
 class InviteNotificationSchema(BaseModel):
+    id: UUID
     project_name: str
     project_creator: str
     date_time: str
+    state: str
 
 
 class MessageNotificationSchema(BaseModel):
+    id: UUID
     message: str
     date_time: str
