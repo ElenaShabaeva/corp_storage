22dd77471fb1f002cbf62a44714f8f09b8fe64b8
Fazzelis <odidk08@gmail.com>
2026-02-26 19:26:39 +0700

    Убрана реализация yjs из fastapi сервера

--- backend/controllers/document_controller.py
+++ backend/controllers/document_controller.py
@@ -1,10 +1,9 @@
-from fastapi import APIRouter, Depends, WebSocket, Query
+from fastapi import APIRouter, Depends
 from fastapi.security import HTTPAuthorizationCredentials
 from configuration import settings
 from services.document_service import DocumentService
 from uuid import UUID
 from schemas.response.document_response import (
-    DocumentResponseSchema,
     DocumentResponseSchema,
     DocumentsResponseSchema
 )
@@ -51,22 +50,4 @@ async def delete(
         access_token=credentials.credentials,
         project_id=project_id,
         document_id=document_id
-    )
-
-
-@router.websocket("/documents/ws/{document_id}")
-async def connect(
-        websocket: WebSocket,
-        document_id: UUID,
-        token: str = Query(...),
-        document_service: DocumentService = Depends(DocumentService)
-):
-    access_token = HTTPAuthorizationCredentials(
-        scheme="Bearer",
-        credentials=token
-    )
-    await document_service.connect(
-        access_token=access_token.credentials,
-        document_id=document_id,
-        websocket=websocket
     )
